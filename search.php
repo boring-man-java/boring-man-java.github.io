75e53784732c2014b1a712803016d65137de5b8b
boring-man-java <75248398+boring-man-java@users.noreply.github.com>
2020-12-14 21:02:09 +0800

    Update search.php

--- search.php
+++ search.php
@@ -1,48 +1,29 @@
-<?php 
-set_time_limit("600"); 
-//获取搜索关键字 
-$keyword=trim($_POST["keyword"]); 
-//检查是否为空 
-if($keyword==""){ 
-echo"您要搜索的关键字不能为空"; 
-exit;//结束程序 
-} 
-function listFiles($dir,$keyword,&$array){ 
-$handle=opendir($dir); 
-while(false!==($file=readdir($handle))){ 
-if($file!="."&&$file!=".."){ 
-if(is_dir("$dir/$file")){ 
-listFiles("$dir/$file",$keyword,$array); 
-} 
-else{ 
-$data=fread(fopen("$dir/$file","r"),filesize("$dir/$file")); 
-if(eregi("<body([^>]+)>(.+)</body>",$data,$b)){ 
-$body=strip_tags($b["2"]); 
-} 
-else{ 
-$body=strip_tags($data); 
-} 
-if($file!="search.php"){ 
-if(eregi("$keyword",$body)){ 
-if(eregi("<title>(.+)</title>",$data,$m)){ 
-$title=$m["1"]; 
-} 
-else{ 
-$title="没有标题"; 
-} 
-$array[]="$dir/$file $title"; 
-} 
-} 
-} 
-} 
-} 
-} 
-$array=array(); 
-listFiles(".","$keyword",$array); 
-foreach($array as $value){ 
-//拆开 
-list($filedir,$title)=split("[ ]",$value,"2"); 
-//输出 
-echo "<a href=$filedir target=_blank>$title </a>"."<br>/n"; 
-} 
+<?php
+header('Content-type:text/json; charset=utf-8');
+@ $db = mysqli_connect('localhost','root','');
+mysqli_query($db,"set names utf8;");
+mysqli_select_db($db,'mpicture');//找到数据库mpicture
+if(mysqli_connect_errno()){
+ echo "Error:Could not connect to mysqli database.";
+ exit;
+}
+$keyword=$_GET['keyword'];
+$q="SELECT count(*) FROM picture where title like '%$keyword%'";
+$result1 = mysqli_query($db,$q);
+$row1 = mysqli_fetch_array($result1);
+$allnum=$row1[0];
+//echo $allnum;
+$k="SELECT * FROM picture where title like '%$keyword%';";//对表picture进行选择
+$result = mysqli_query($db,$k);//执行$k的mysql语句，并赋给result
+$rownum = mysqli_num_rows($result);//获取result的数据数量
+$picture=array(array());
+for($i=0;$i<$rownum;$i++){
+    $row = mysqli_fetch_assoc($result);//获取result的一条数据
+	$picture[0][$i]= $row['path1'];//获得缩略图的路径
+	$picture[1][$i]= $row['path2'];//获得全图的路径
+	$picture[2][$i]= $row['title'];//获得文字描述
+	$picture[3][$i]= $row['Id'];//获得id
+}
+$picture[4][0]=$allnum;
+echo json_encode($picture);
 ?>
