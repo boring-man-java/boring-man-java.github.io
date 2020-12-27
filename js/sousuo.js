9201b89f5baec1c7ca6932c6b771b1fc3f91c394
boring-man-java <75248398+boring-man-java@users.noreply.github.com>
2020-12-28 05:41:25 +0800

    Create sousuo.js

---
+++ js/sousuo.js
@@ -0,0 +1,11 @@
+type="text/javascript"
+var searchStyle = document.getElementById('search_style');
+document.getElementById('search').addEventListener('input', function() {
+  if (!this.value) {
+    searchStyle.innerHTML = "";
+    return;
+  }
+  // look ma, no indexOf!
+  searchStyle.innerHTML = ".searchable:not([data-index*=\"" + this.value.toLowerCase() + "\"]) { display: none; }";
+  // beware of css injections!
+});
